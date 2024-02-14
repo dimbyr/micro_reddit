6a10d76ef45070299514884bded5c71605cb92c3
Dimby Rabearivony <drabearivony@gmail.com>
2024-02-14 16:32:24 +0200

    Completed as required by TOP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, presence: true
   validates :username, uniqueness: true 
   validates :username, length: { minimum: 5, maximum: 20, message: "Length out of range" }
